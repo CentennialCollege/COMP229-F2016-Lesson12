086d54210bae11340cc36a21e468e571d09e99d1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-30 22:18:41 -0500

    updated Shopping with working AddToCart Button

--- COMP229-F2016-Lesson12/App_Start/BundleConfig.cs
+++ COMP229-F2016-Lesson12/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace COMP229_F2016_Lesson12
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
