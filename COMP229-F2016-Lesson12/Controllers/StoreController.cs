7a4447d282b7ca226f008fecab2aa70ff9e1c1c4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-30 19:52:12 -0500

    Initial Commit

---
+++ COMP229-F2016-Lesson12/Controllers/StoreController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using COMP229_F2016_Lesson12.Models;
+
+namespace COMP229_F2016_Lesson12.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            /*
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre("Disco"),
+                new Genre("Jazz"),
+                new Genre("Rock"),
+                new Genre("Electronic")
+            }; */
+
+            return View(db.Genres.ToList());
+        }
+
+        // GET: Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            if (genre == null)
+            {
+                genre = "Disco";
+            }
+
+            Genre genreModel = db.Genres.Include("Albums").Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int? id = 1)
+        {
+            Album album = db.Albums.Find(id);
+
+            return View(album);
+        }
+
+
+    }
+}
---
+++ COMP229-F2016-Lesson12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP229_F2016_Lesson12.Startup))]
+namespace COMP229_F2016_Lesson12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
