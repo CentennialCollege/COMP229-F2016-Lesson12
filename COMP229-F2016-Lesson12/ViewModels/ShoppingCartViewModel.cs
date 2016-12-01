4b995ca05c5c456587bdeeb0b542bff801b0acda
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-30 21:39:15 -0500

    added ShoppingCart model and related controllers

---
+++ COMP229-F2016-Lesson12/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using COMP229_F2016_Lesson12.Models;
+
+namespace COMP229_F2016_Lesson12.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
